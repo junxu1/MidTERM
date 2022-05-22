543f85900404aa124dfbbb8e57977349d0911db3
junxu1 <C110156124@nkust.edu.tw>
2022-05-22 17:34:11 +0800

    0522

---
+++ p54.py
@@ -0,0 +1,10 @@
+a1=float(input("請輸入路程公里數(km"))
+a2=float(input("請輸入路程公里數(km"))
+a3=float(input("請輸入路程公里數(km"))
+a4=float(input("請輸入路程公里數(km"))
+a5=float(input("請輸入路程公里數(km"))
+if a1<1.5:
+    print("所需車資為:",75)
+elif a2>1.5:
+    print("所需車資為:",75+)
+
